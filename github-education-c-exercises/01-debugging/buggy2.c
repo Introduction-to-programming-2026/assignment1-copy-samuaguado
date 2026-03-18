ff83cf8ed58c8c77471e9bf43bc9faf0d774a2c6
Samuel Aguado <pineiro.samuel@ibu.edu.mk>
2026-03-18 19:57:53 +0100

    Assignment 1

--- github-education-c-exercises/01-debugging/buggy2.c
+++ github-education-c-exercises/01-debugging/buggy2.c
@@ -1,8 +1,10 @@
 // Missing cs50.h, variable's type, semicolon, %s, and second printf argument.
+//#include <cs50.h> (Im from VS Code and does not recognize this library)
 #include <stdio.h>
-
 int main(void)
 {
-    name = get_string("What's your name? ")
-    printf("hello, world\n");
+    char name[50];
+    printf("What is your name?\n");
+    scanf ("%s", name);
+    printf("hello, %s\n", name);
 }
